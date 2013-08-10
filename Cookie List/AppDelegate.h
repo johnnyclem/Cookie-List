792a1674131c3307804ccdfaf3e8cfa9c1dd6a8e
John Clem <johnnyclem@gmail.com>
2013-08-09 21:39:56 -0700

    initial commit

---
+++ Cookie List/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cookie List
+//
+//  Created by John Clem on 8/9/13.
+//  Copyright (c) 2013 Code Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cookie List/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cookie List
+//
+//  Created by John Clem on 8/9/13.
+//  Copyright (c) 2013 Code Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
